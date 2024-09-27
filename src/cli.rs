40e969859cfc6d5e9f89b854c049086ec8da1448
Jens Krumsieck <j.krumsieck@outlook.de>
2024-09-27 07:03:51 +0000

    build initial structure

---
+++ src/cli.rs
@@ -0,0 +1,38 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name="s4n", about="Client tool for Scientific Workflow Infrastructure (SciWIn)", long_about=None, version)]
+pub(crate) struct Cli {
+    #[command(subcommand)]
+    pub(crate) command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub(crate) enum Commands {
+    //temporary dummy command to showcase clap usage
+    Dummy {
+        #[command(subcommand)]
+        command: DummyCommands,
+    },
+    Tool,
+    Workflow,
+    Annotate,
+    Execute,
+    Sync,
+}
+
+#[derive(Debug, Subcommand)]
+pub(crate) enum DummyCommands {
+    #[command(about = "Creates a dummy")]
+    Create(CreateDummyArgs),
+    Read,
+    Update,
+    Delete,
+}
+
+#[derive(Args, Debug)]
+pub(crate) struct CreateDummyArgs {
+    name: String,
+    #[arg(short = 'o', long = "option")]
+    option: Option<String>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod cli;
+use cli::{Cli, Commands, CreateDummyArgs, DummyCommands};
+
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match &args.command {
+        Commands::Dummy { command } => match command {
+            DummyCommands::Create(args) => create_dummy(args),
+            DummyCommands::Read => todo!(),
+            DummyCommands::Update => todo!(),
+            DummyCommands::Delete => todo!(),
+        },
+        Commands::Tool => todo!(),
+        Commands::Workflow => todo!(),
+        Commands::Annotate => todo!(),
+        Commands::Execute => todo!(),
+        Commands::Sync => todo!(),
+    }
+}
+
+fn create_dummy(args: &CreateDummyArgs) {
+    println!("Dummy creation called with {:?}", args);
 }
