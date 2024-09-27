37b68adc95ea5e4744fab766eabca1729b29c527
Jens Krumsieck <j.krumsieck@outlook.de>
2024-09-27 08:46:54 +0000

    parse cwls base command

--- src/tool/parser.rs
+++ src/tool/parser.rs
@@ -1,3 +1,21 @@
-pub fn parse_command_line(str: Vec<String>) {
-    println!("{:?}", str);
+//TODO: complete list
+static SCRIPT_EXECUTORS: &[&str] = &["python", "Rscript"];
+
+pub fn parse_command_line(command: Vec<String>) {
+    let base_command = get_base_command(command);
+    println!("{:?}", base_command);
+}
+
+fn get_base_command(command: Vec<String>) -> Vec<String> {
+    if command.is_empty() {
+        return Vec::new();
+    };
+
+    let mut base_command = vec![command[0].clone()];
+
+    if SCRIPT_EXECUTORS.contains(&command[0].as_str()) {
+        base_command.push(command[1].clone());
+    }
+
+    return base_command;
 }
