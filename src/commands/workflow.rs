fcae2b594a749d2fd0724fc842d42881b922f7f6
Jens Krumsieck <j.krumsieck@outlook.de>
2024-11-04 15:27:33 +0000

    started implementation of connect command

--- src/commands/workflow.rs
+++ src/commands/workflow.rs
@@ -9,6 +9,7 @@ use crate::{
 pub fn handle_workflow_commands(command: &WorkflowCommands) -> Result<(), Box<dyn Error>> {
     match command {
         WorkflowCommands::Create(args) => create_workflow(args)?,
+        WorkflowCommands::Connect(args) => connect_workflow_nodes(args)?,
     }
     Ok(())
 }
@@ -17,6 +18,8 @@ pub fn handle_workflow_commands(command: &WorkflowCommands) -> Result<(), Box<dy
 pub enum WorkflowCommands {
     #[command(about = "Creates a blank workflow")]
     Create(CreateWorkflowArgs),
+    #[command(about = "Connects a workflow node")]
+    Connect(ConnectWorkflowArgs),
 }
 
 #[derive(Args, Debug)]
@@ -35,5 +38,20 @@ pub fn create_workflow(args: &CreateWorkflowArgs) -> Result<(), Box<dyn Error>> 
 
     create_and_write_file(&filename, &yaml).map_err(|e| format!("Could not create workflow {}: {}", args.name, e))?;
 
+    Ok(())
+}
+
+#[derive(Args, Debug)]
+pub struct ConnectWorkflowArgs {
+    #[arg(help = "Name of the workflow name to be altered")]
+    pub name: String,
+    #[arg(short = 'f', long = "from", help = "Starting Node: [tool]/[output]")]
+    pub from: String,
+    #[arg(short = 't', long = "to", help = "Ending Node: [tool]/[input]")]
+    pub to: String,
+}
+
+pub fn connect_workflow_nodes(args: &ConnectWorkflowArgs) -> Result<(), Box<dyn Error>> {
+    println!("{:#?}", args);
     Ok(())
 }
