68ba8f53b1ae6e83d57d37099882bea220afc8a5
Jens Krumsieck <j.krumsieck@outlook.de>
2024-11-08 10:30:24 +0000

    added integration tests for execution

--- tests/execute_integration_test.rs
+++ tests/execute_integration_test.rs
@@ -1,6 +1,7 @@
 use s4n::commands::execute::{execute_local, LocalExecuteArgs, Runner};
 use serial_test::serial;
 use std::{fs, path::Path};
+use tempfile::tempdir;
 
 #[test]
 #[serial]
@@ -73,6 +74,65 @@ pub fn test_execute_local_with_file() {
     let expected = fs::read_to_string("tests/test_data/input_alt.txt").unwrap();
 
     assert_eq!(contents, expected);
-    
+
     fs::remove_file(file).unwrap();
+}
+
+#[test]
+#[serial]
+pub fn test_execute_local_outdir() {
+    let dir = tempdir().unwrap();
+    let args = LocalExecuteArgs {
+        runner: Runner::Custom,
+        out_dir: Some(dir.path().to_string_lossy().into_owned()),
+        is_quiet: false,
+        file: "tests/test_data/echo.cwl".to_string(),
+        args: vec![],
+    };
+
+    execute_local(&args).expect("Could not execute CommandLineTool");
+
+    let file = dir.path().join("results.txt");
+    assert!(file.exists());
+    fs::remove_file(file).unwrap();
+}
+
+#[test]
+#[serial]
+pub fn test_execute_local_is_quiet() {
+    //does not really test if it is quiet but rather that the process works
+    let args = LocalExecuteArgs {
+        runner: Runner::Custom,
+        out_dir: None,
+        is_quiet: true,
+        file: "tests/test_data/echo.cwl".to_string(),
+        args: vec![],
+    };
+
+    execute_local(&args).expect("Could not execute CommandLineTool");
+
+    let file = Path::new("results.txt");
+    assert!(file.exists());
+    fs::remove_file(file).unwrap();
+}
+
+#[test]
+#[serial]
+pub fn test_execute_local_cwltool() {
+    //as cwltool does not support windows, we can not test that
+    if !cfg!(target_os = "windows") {
+        let args = LocalExecuteArgs {
+            runner: Runner::CWLTool,
+            out_dir: None,
+            is_quiet: false,
+            file: "tests/test_data/echo.cwl".to_string(),
+            args: vec![],
+        };
+
+        execute_local(&args).expect("Could not execute CommandLineTool");
+
+        let file = Path::new("results.txt");
+        assert!(file.exists());
+        fs::remove_file(file).unwrap();
+    }
 }
