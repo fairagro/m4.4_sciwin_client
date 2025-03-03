6ab67d90f34dba6eeabdb5dd6ed071aea7800460
Jens Krumsieck <j.krumsieck@outlook.de>
2025-03-03 14:55:56 +0000

    link new proj

--- src/commands/execute.rs
+++ src/commands/execute.rs
@@ -14,6 +14,7 @@ use cwl::{
     wf::Workflow,
     CWLDocument,
 };
+use cwl_execution::execute;
 use log::info;
 use serde_yaml::{Number, Value};
 use std::{
@@ -103,8 +104,8 @@ pub fn execute_local(args: &LocalExecuteArgs) -> Result<(), Box<dyn Error>> {
             }
 
             //gather inputs
-            let contents = fs::read_to_string(&args.file).map_err(|e| format!("Could not load File {:?}: {}", args.file, e))?;
-            let mut inputs: Option<HashMap<String, DefaultValue>> = None;
+            let job_contents = fs::read_to_string(&args.file).map_err(|e| format!("Could not load File {:?}: {}", args.file, e))?;
+            let mut inputs: HashMap<String, DefaultValue> = HashMap::new();
             let is_file_input = args.args.len() == 1 && !&args.args[0].starts_with("-");
 
             //check for yaml input
@@ -114,13 +115,11 @@ pub fn execute_local(args: &LocalExecuteArgs) -> Result<(), Box<dyn Error>> {
                     let input = &args.args[0];
                     if is_file_input {
                         let yaml = fs::read_to_string(input).map_err(|e| format!("Could not load File {}: {}", input, e))?;
-                        inputs = Some(serde_yaml::from_str(&yaml).map_err(|e| format!("Could not read input file: {}", e))?);
+                        inputs = serde_yaml::from_str(&yaml).map_err(|e| format!("Could not read input file: {}", e))?;
                     }
                 }
                 //arguments given as commandline inputs
                 n if n > 1 => {
-                    inputs = Some(HashMap::new());
-                    let map = inputs.as_mut().unwrap();
                     let mut i = 0;
                     while i < args.args.len() {
                         if args.args[i].starts_with("-") {
@@ -131,7 +130,7 @@ pub fn execute_local(args: &LocalExecuteArgs) -> Result<(), Box<dyn Error>> {
                                 CWLType::Directory => DefaultValue::Directory(Directory::from_location(raw_value)),
                                 _ => serde_yaml::from_str(&args.args[i + 1])?,
                             };
-                            map.insert(key, value);
+                            inputs.insert(key, value);
                             i += 1;
                         }
                         i += 1;
@@ -141,56 +140,45 @@ pub fn execute_local(args: &LocalExecuteArgs) -> Result<(), Box<dyn Error>> {
                 _ => {}
             }
 
-            fn correct_path<T: PathItem>(item: &mut T, path_prefix: &Path) {
-                let location = item.get_location().clone();
-                item.set_location(join_path_string(path_prefix, &location));
-                if let Some(secondary_files) = item.secondary_files_mut() {
-                    for sec_file in secondary_files {
-                        match sec_file {
-                            DefaultValue::File(file) => {
-                                file.set_location(join_path_string(path_prefix, &file.get_location()));
+            //make paths relative to calling object
+            let path_prefix = if is_file_input {
+                Path::new(&args.args[0]).parent().unwrap()
+            } else {
+                Path::new(".")
+            };
+            for value in inputs.values_mut() {
+                match value {
+                    DefaultValue::File(file) => {
+                        let location = file.get_location().clone();
+                        file.location = Some(join_path_string(path_prefix, &location));
+                        if let Some(secondary_files) = &mut file.secondary_files {
+                            for sec_file in secondary_files.iter_mut() {
+                                match sec_file {
+                                    DefaultValue::File(file) => {
+                                        file.set_location(join_path_string(path_prefix, &file.get_location()));
+                                    }
+                                    DefaultValue::Directory(directory) => {
+                                        directory.set_location(join_path_string(path_prefix, &directory.get_location()))
+                                    }
+                                    DefaultValue::Any(_) => (),
+                                }
                             }
-                            DefaultValue::Directory(directory) => directory.set_location(join_path_string(path_prefix, &directory.get_location())),
-                            DefaultValue::Any(_) => (),
                         }
                     }
-                }
-            }
-
-            //make paths relative to calling object
-            if let Some(inputs) = &mut inputs {
-                let path_prefix = if is_file_input {
-                    Path::new(&args.args[0]).parent().unwrap()
-                } else {
-                    Path::new(".")
-                };
-                for value in inputs.values_mut() {
-                    match value {
-                        DefaultValue::File(file) => correct_path(file, path_prefix),
-                        DefaultValue::Directory(directory) => correct_path(directory, path_prefix),
-                        DefaultValue::Any(_) => (),
+                    DefaultValue::Directory(directory) => {
+                        let location = directory.get_location().clone();
+                        directory.location = Some(join_path_string(path_prefix, &location));
                     }
+                    DefaultValue::Any(_) => (),
                 }
             }
 
-            //preprocess cwl import statements
-            let preprocessed_contents = preprocess_cwl(&contents, &args.file);
+            let cwl = &args.file;
+            let outdir = args.out_dir.clone();
 
-            let cwl_yaml: Value = serde_yaml::from_str(&preprocessed_contents).map_err(|e| format!("Could not load YAML: {}", e))?;
-            let class = cwl_yaml.get("class").expect("Could not get class").as_str().unwrap();
-            let is_workflow = class == "Workflow";
-            let is_tool = class == "CommandLineTool";
-            if is_tool {
-                let mut tool: CommandLineTool = serde_yaml::from_value(cwl_yaml).map_err(|e| format!("Could not load CommandLineTool: {}", e))?;
-                run_commandlinetool(&mut tool, inputs, Some(&args.file), args.out_dir.clone())?;
-            } else if is_workflow {
-                let mut workflow: Workflow = serde_yaml::from_value(cwl_yaml).map_err(|e| format!("Could not load Workflow: {}", e))?;
-                run_workflow(&mut workflow, inputs, Some(&args.file), args.out_dir.clone())?;
-            } else {
-                Err(CommandError {
-                    exit_code: 33,
-                    message: format!("CWL Document of class {class:?} is not supported"),
-                })?
+            if let Ok(outputs) = execute(&cwl, inputs, outdir) {
+                let json = serde_json::to_string_pretty(&outputs)?;
+                println!("{json}");
             }
 
             Ok(())
@@ -205,6 +193,7 @@ pub fn make_template(filename: &PathBuf) -> Result<(), Box<dyn Error>> {
     let inputs = match cwl {
         CWLDocument::CommandLineTool(tool) => tool.inputs,
         CWLDocument::Workflow(workflow) => workflow.inputs,
+        CWLDocument::ExpressionTool(expression_tool) => expression_tool.inputs,
     };
 
     let template = inputs
