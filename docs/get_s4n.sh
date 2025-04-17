81ab0be822ad710a244bbf6f496dc6e5ed15dcc5
Jens Krumsieck <j.krumsieck@outlook.de>
2025-04-17 16:19:42 +0200

    add condition

--- docs/get_s4n.sh
+++ docs/get_s4n.sh
@@ -4,7 +4,11 @@ set -u
 
 url="https://api.github.com/repos/fairagro/m4.4_sciwin_client/releases/latest"
 
-tag=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "$url" | jq -r ".tag_name")
+if [ -n "${GITHUB_TOKEN:-}" ]; then
+    tag=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "$url" | jq -r ".tag_name")
+else
+    tag=$(curl -s "$url" | jq -r ".tag_name")
+fi
 
 if [ -z "$tag" ]; then
     echo "Invalid Response"
