cd2882f325a754feeea0d6ccb6ea922507109055
Jens Krumsieck <j.krumsieck@outlook.de>
2025-05-05 10:40:40 +0200

    added some art :)

--- src/cli.rs
+++ src/cli.rs
@@ -10,7 +10,15 @@ use clap_complete::{generate, Generator, Shell};
 use std::{error::Error, io};
 
 #[derive(Parser, Debug)]
-#[command(name="s4n", about="Client tool for Scientific Workflow Infrastructure (SciWIn)", long_about=None, version)]
+#[command(name="s4n", about=r#"
+ _____        _  _    _  _____         _____  _  _               _   
+/  ___|      (_)| |  | ||_   _|       /  __ \| |(_)             | |  
+\ `--.   ___  _ | |  | |  | |  _ __   | /  \/| | _   ___  _ __  | |_ 
+ `--. \ / __|| || |/\| |  | | | '_ \  | |    | || | / _ \| '_ \ | __|
+/\__/ /| (__ | |\  /\  / _| |_| | | | | \__/\| || ||  __/| | | || |_ 
+\____/  \___||_| \/  \/  \___/|_| |_|  \____/|_||_| \___||_| |_| \__|
+Client tool for Scientific Workflow Infrastructure (SciWIn)"#
+, long_about=None, version)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
