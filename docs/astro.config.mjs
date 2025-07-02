05dd111a84985d61b027e73f0f2618f4e40bfe38
Jens Krumsieck <j.krumsieck@outlook.de>
2025-07-02 12:49:39 +0200

    started landing page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,13 +2,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'SciWIn Client',
+      customCss: [
+        '@fontsource/fira-sans/400.css',
+        '@fontsource/fira-sans/700.css',
+        '@fontsource/fira-sans/900.css',
+        '@fontsource/fira-sans/400-italic.css',
+        '@fontsource/fira-sans/700-italic.css',
+        '@fontsource/fira-sans/900-italic.css',
+        './src/styles/global.css'
+      ],
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/fairagro/sciwin_client' }],
+      components: {
+        Hero: './src/components/Hero.astro',
+      }
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
