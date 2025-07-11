7d4d8003a5eeeff929ec5088f3a8e0c8ca066371
Jens Krumsieck <j.krumsieck@outlook.de>
2025-07-11 10:22:42 +0200

    small changs

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,33 +1,67 @@
-use std::fs;
-
-use eframe::{egui, NativeOptions};
-
-use crate::commands::tool::{create_tool, CreateToolArgs};
+use eframe::{
+    egui::{self, CentralPanel, SidePanel},
+    NativeOptions,
+};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
+use walkdir::WalkDir;
 
 pub fn main() -> eframe::Result {
     let options = NativeOptions { ..Default::default() };
 
-    let mut command = String::new();
-    let mut cwl = String::new();
+    eframe::run_native("SciWIn Client", options, Box::new(|_| Ok(Box::<App>::default())))
+}
 
-    eframe::run_simple_native("SciWIn Client", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("This is a test!");
-            ui.text_edit_singleline(&mut command);
+struct App {
+    workdir: PathBuf,
+    current_file: Option<PathBuf>,
+}
 
-            if ui.button("Fire!").clicked() {
-                let opts = CreateToolArgs {
-                    command: shlex::split(&command).unwrap(),
-                    ..Default::default()
-                };
-                if create_tool(&opts).is_ok() {
-                    let file = &opts.command[0];
-                    let file = format!("workflows/{file}/{file}.cwl");
-                    cwl = fs::read_to_string(file).unwrap_or("Failed to read!".to_string());
+impl App {
+    fn open_file(&mut self, filename: impl AsRef<Path>) {
+        self.current_file = Some(filename.as_ref().to_path_buf());
+    }
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            workdir: env::current_dir().unwrap(),
+            current_file: Default::default(),
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        catppuccin_egui::set_theme(ctx, catppuccin_egui::LATTE);
+        SidePanel::left("file_sidebar").resizable(true).default_width(200.0).show(ctx, |ui| {
+            ui.heading("Files");
+            ui.separator();
+
+            for entry in WalkDir::new(&self.workdir).into_iter().filter_map(Result::ok) {
+                if entry.file_type().is_file() {
+                    let file_name = entry.file_name().to_string_lossy();
+                    if let Some(cwl_doc) = file_name.strip_suffix(".cwl") {
+                        if ui.button(cwl_doc).clicked() {
+                            self.open_file(entry.path());
+                        }
+                    }
                 }
             }
-
-            ui.text_edit_multiline(&mut cwl);
         });
-    })
+
+        // Main editor area
+        CentralPanel::default().show(ctx, |ui| {
+            if let Some(current_file) = &self.current_file {
+                ui.heading(current_file.file_stem().and_then(|s| s.to_str()).unwrap_or("Unknown file"));
+                ui.separator();
+                ui.label(format!("Editing: {current_file:?}"));
+            } else {
+                ui.label("No file selected.");
+            }
+        });
+    }
 }
