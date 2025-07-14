bce18fb8dc493b05be81b62cbe4056c391b97d66
Jens Krumsieck <j.krumsieck@outlook.de>
2025-07-14 14:54:32 +0200

    test

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,16 +1,31 @@
-use std::{error::Error, rc::Rc};
-
+use crate::io::get_workflows_folder;
 use slint::{ComponentHandle, ModelRc, VecModel};
+use std::{env, error::Error, path::Path, rc::Rc};
 
 slint::include_modules!();
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     let ui = MainWindow::new()?;
-    
-    let files = Rc::new(VecModel::from(vec!["tool.cwl".into(), "workflows/main.cwl".into()]));
-    ui.set_file_list(ModelRc::from(files));
+
+    let files = read_files(&env::current_dir()?)?;
+    let s_files: Vec<slint::SharedString> = files.into_iter().map(|s| s.into()).collect();
+    let rc_files = Rc::new(VecModel::from(s_files));
+    ui.set_file_list(ModelRc::from(rc_files));
 
     ui.run()?;
 
     Ok(())
+}
+
+fn read_files(project_dir: &Path) -> Result<Vec<String>, Box<dyn Error>> {
+    let workflow_dir = project_dir.join(get_workflows_folder());
+    let mut files = vec![];
+
+    for entry in walkdir::WalkDir::new(workflow_dir) {
+        let entry = entry?;
+        if entry.file_type().is_file() && entry.path().extension().map(|ext| ext == "cwl").unwrap_or(false) {
+            files.push(entry.path().to_string_lossy().into_owned());
+        }
+    }
+    Ok(files)
 }
