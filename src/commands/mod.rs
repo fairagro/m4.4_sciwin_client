229e461b98156e2976a1cd2bacfca023a9df4a22
JensKrumsieck <j.krumsieck@outlook.de>
2025-07-23 15:09:51 +0200

    add colorful theme here

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,4 @@
-use dialoguer::Input;
+use dialoguer::{theme::ColorfulTheme, Input};
 use git2::Config;
 use log::warn;
 use std::error::Error;
@@ -20,10 +20,14 @@ pub fn check_git_config() -> Result<(), Box<dyn Error>> {
     if config.get_string("user.name").is_err() || config.get_string("user.email").is_err() {
         warn!("User configuration not found!");
 
-        let name: String = Input::new().with_prompt("Enter your name").interact_text()?;
+        let name: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Enter your name")
+            .interact_text()?;
         config.set_str("user.name", name.trim())?;
 
-        let mail: String = Input::new().with_prompt("Enter your email").interact_text()?;
+        let mail: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("Enter your email")
+            .interact_text()?;
         config.set_str("user.email", mail.trim())?;
     }
     Ok(())
