8bd90b905c7c8a87813eb2c2c1b2c86e3eb90fb7
JensKrumsieck <j.krumsieck@outlook.de>
2025-08-05 10:05:53 +0200

    debug

--- crates/cwl/src/packed.rs
+++ crates/cwl/src/packed.rs
@@ -362,7 +362,7 @@ mod tests {
         pack_input(&mut input, "#calculation.cwl", file_path);
 
         let json = serde_json::json!(&input);
-
+        println!("{json:#?}");
         let reference_json = r##"{
                     "id": "#calculation.cwl/population",
                     "type": "File",
