77fef45747f0abc28d2a5375557538c91c87705c
Jens Krumsieck <j.krumsieck@outlook.de>
2025-11-10 16:14:18 +0100

    minor changes

--- packages/gui/src/graph.rs
+++ packages/gui/src/graph.rs
@@ -1,30 +1,53 @@
-use std::collections::HashMap;
-
-use commonwl::{StringOrDocument, prelude::*};
+use commonwl::{StringOrDocument, load_doc, load_workflow, prelude::*};
 use petgraph::{graph::NodeIndex, prelude::StableDiGraph};
+use std::{collections::HashMap, path::Path};
 
+#[derive(Debug, Clone)]
 pub enum Node {
     Step(CWLDocument),
     Input(CommandInputParameter), //WorkflowInputParameter
     Output(WorkflowOutputParameter),
 }
 
+impl Node {
+    pub fn id(&self) -> String {
+        match &self {
+            Self::Step(doc) => doc.id.clone().unwrap().clone(),
+            Self::Input(input) => input.id.clone(),
+            Self::Output(output) => output.id.clone(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct Edge {
     pub source_port: String,
     pub target_port: String,
     pub data_type: CWLType,
 }
 
-type WorkflowGraph = StableDiGraph<Node, Edge>;
+pub type WorkflowGraph = StableDiGraph<Node, Edge>;
+
+pub fn load_workflow_graph(path: impl AsRef<Path>) -> anyhow::Result<WorkflowGraph> {
+    let workflow = load_workflow(path.as_ref()).map_err(|e| anyhow::anyhow!("{e}"))?;
+    let wgb = WorkflowGraphBuilder::from_workflow(&workflow, path)?;
+    Ok(wgb.graph)
+}
 
 #[derive(Default)]
-pub struct WorkflowGraphBuilder {
+struct WorkflowGraphBuilder {
     pub graph: WorkflowGraph,
     node_map: HashMap<String, NodeIndex>,
 }
 
 impl WorkflowGraphBuilder {
-    pub fn from_workflow(&mut self, workflow: &Workflow) -> anyhow::Result<()> {
+    fn from_workflow(workflow: &Workflow, path: impl AsRef<Path>) -> anyhow::Result<Self> {
+        let mut builder = Self::default();
+        builder.load_workflow(workflow, path)?;
+        Ok(builder)
+    }
+
+    fn load_workflow(&mut self, workflow: &Workflow, path: impl AsRef<Path>) -> anyhow::Result<()> {
         for input in &workflow.inputs {
             let node_id = self.graph.add_node(Node::Input(input.clone()));
             self.node_map.insert(input.id.clone(), node_id);
@@ -34,12 +57,21 @@ impl WorkflowGraphBuilder {
             let node_id = self.graph.add_node(Node::Output(output.clone()));
             self.node_map.insert(output.id.clone(), node_id);
         }
+        let step_ids = workflow.sort_steps().map_err(|e| anyhow::anyhow!("{e}"))?;
 
-        for step in &workflow.steps {
-            let StringOrDocument::Document(doc) = &step.run else {
-                anyhow::bail!("String not supported")
+        for step_id in step_ids {
+            let step = workflow.get_step(&step_id).unwrap();
+            let StringOrDocument::String(str) = &step.run else {
+                anyhow::bail!("Inline Document not supported")
             };
-            let node_id = self.graph.add_node(Node::Step(*doc.clone()));
+
+            let step_file = path.as_ref().parent().unwrap().join(str);
+            let mut doc = load_doc(&step_file).map_err(|e| anyhow::anyhow!("{e}"))?;
+            if doc.id.is_none() {
+                doc.id = Some(step_file.file_name().unwrap().to_string_lossy().to_string());
+            }
+
+            let node_id = self.graph.add_node(Node::Step(doc.clone()));
             self.node_map.insert(step.id.clone(), node_id);
 
             for wsip in &step.in_ {
--- packages/gui/src/main.rs
+++ packages/gui/src/main.rs
@@ -1,4 +1,16 @@
 use dioxus::prelude::*;
+use gui::graph::{WorkflowGraph, load_workflow_graph};
+use petgraph::graph::NodeIndex;
+use petgraph::visit::IntoNodeIdentifiers;
+
+#[derive(Default, Clone)]
+pub struct ApplicationState {
+    graph: WorkflowGraph,
+}
+
+pub fn use_app_state() -> Signal<ApplicationState> {
+    use_context::<Signal<ApplicationState>>()
+}
 
 fn main() {
     dioxus::launch(App);
@@ -6,11 +18,14 @@ fn main() {
 
 #[component]
 fn App() -> Element {
+    use_context_provider(|| Signal::new(ApplicationState::default()));
     rsx! {
         document::Link { rel: "icon", href:  asset!("/assets/favicon.ico") }
         document::Stylesheet {  href: asset!("/assets/main.css") }
         document::Stylesheet { href: asset!("/assets/tailwind.css") }
-        Logo {}
+        Logo { }
+        Load_Btn {  }
+        Graph {  }
     }
 }
 
@@ -18,7 +33,61 @@ fn App() -> Element {
 pub fn Logo() -> Element {
     rsx! {
         div {
-            img { src: asset!("/assets/logo.svg"), width: 200 }
+            img { src: asset!("/assets/logo.svg"), width: 150 }
+        }
+    }
+}
+
+#[component]
+pub fn Load_Btn() -> Element {
+    rsx! {
+        button {
+            class: "rounded-lg bg-green-500 px-3 py-1 my-5 cursor-pointer",
+            onclick: move |_| {
+                let graph = load_workflow_graph("testdata/hello_world/workflows/main/main.cwl")?;
+                use_app_state().write().graph = graph;
+                Ok(())
+            },
+            "Load CWL"
+        }
+    }
+}
+
+#[component]
+pub fn Graph() -> Element {
+    let graph = use_app_state()().graph;
+
+    rsx! {
+        div {
+            div {
+                class: "relative h-full w-full",
+                for id in graph.node_identifiers() {
+                    Node {id}
+                },
+            },
+            div {
+                "Debug: {graph:?}"
+            }
+        }
+    }
+}
+
+#[derive(Props, Clone, Copy, PartialEq)]
+pub struct NodeProps {
+    id: NodeIndex,
+}
+
+#[component]
+pub fn Node(props: NodeProps) -> Element {
+    let graph = use_app_state()().graph;
+    let node = &graph[props.id];
+    let pos_x = props.id.index() * 100;
+
+    rsx! {
+        div {
+            class: "absolute",
+            left: "{pos_x}px",
+            "{node.id()}"
         }
     }
 }
