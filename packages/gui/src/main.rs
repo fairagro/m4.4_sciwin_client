89fe73f0b6aa732e44bdc104c437ae5f6a79ca99
JensKrumsieck <j.krumsieck@outlook.de>
2025-11-10 11:27:24 +0100

    add logo

--- packages/gui/src/main.rs
+++ packages/gui/src/main.rs
@@ -1,9 +1,5 @@
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
-
 fn main() {
     dioxus::launch(App);
 }
@@ -11,29 +7,19 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Stylesheet {  href: MAIN_CSS } 
+        document::Link { rel: "icon", href:  asset!("/assets/favicon.ico") }
+        document::Stylesheet {  href: asset!("/assets/main.css") }
         document::Stylesheet { href: asset!("/assets/tailwind.css") }
-        Hero {}
+        Logo {}
 
     }
 }
 
 #[component]
-pub fn Hero() -> Element {
+pub fn Logo() -> Element {
     rsx! {
         div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                draggable: "true",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+            img { src: asset!("/assets/logo.svg"), width: 200 }
         }
     }
 }
