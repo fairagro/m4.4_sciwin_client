12005ba13ae7634966859af9a7d1f82b25e8de7b
Jens Krumsieck <j.krumsieck@outlook.de>
2025-12-05 07:40:28 +0100

    fmt

--- packages/gui/src/components/button.rs
+++ packages/gui/src/components/button.rs
@@ -26,16 +26,14 @@ pub fn RoundActionButton(props: ButtonProps) -> Element {
     }
 }
 
-
 #[component]
 pub fn SmallRoundActionButton(props: ButtonProps) -> Element {
-    
     let title = props.title.unwrap_or_default();
     let class = props.class.unwrap_or_default();
     rsx! {
 
         button {
-            class: "p-1  rounded-full hover:rotate-20 transition-[rotate] duration-200 {class}",
+            class: "p-1 rounded-full hover:rotate-20 transition-[rotate] duration-200 {class}",
             onclick: move |e| {
                 if let Some(handler) = props.onclick {
                     handler.call(e);
